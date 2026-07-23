77b6226e60f2b620fc43a2196a996bcffff7847c
Veer Harischandrakar <1012411025@despu.edu.in>
2026-07-23 14:45:30 +0530

    Update histogram.py

--- histogram.py
+++ histogram.py
@@ -1,16 +1,73 @@
-import numpy as np
-
 gray_level = np.arange(8)
 
-pixels = np.array([0,8,8,2,0,7,0,0])
+pixels = []
+
+print("Enter number of pixels: ")
+
+for i in range(len(gray_level)):
+    value = int(input(f"Gray level {gray_level[i]}: "))
+    pixels.append(value)
+
 total_pixels = sum(pixels)
 
-pdf = pixels / total_pixels
-cdf = np.cumsum(pdf)
-skx7 = cdf * 7
+pdf = []
 
-histogram_level = np.round(skx7).astype(int)
+for i in range(len(pixels)):
+    pdf.append(pixels[i] / total_pixels)
 
-print("Gray\tPixels\tPDF\tCDF\tNew Level")
-for i in range(8):
-    print(f"{gray_level[i]}\t{pixels[i]}\t{pdf[i]:.2f}\t{cdf[i]:.2f}\t{histogram_level[i]}")
+cdf = []
+cumulative = 0
+
+for i in range(len(pdf)):
+    cumulative += pdf[i]
+    cdf.append(cumulative)
+
+
+skx7 = []
+
+for i in range(len(cdf)):
+    skx7.append(cdf[i] * 7)
+
+histogram_levels = []
+for i in range(len(skx7)):
+    histogram_levels.append(round(skx7[i]))
+
+
+print("\nGray\tPixels\tPDF\tCDF\tSk×7\tNew Level")
+
+for i in range(len(gray_level)):
+    print(f"{gray_level[i]}\t{pixels[i]}\t{pdf[i]:.2f}\t{cdf[i]:.2f}\t{skx7[i]:.2f}\t{histogram_levels[i]}")
+
+
+original_histogram = []
+
+for i in range(len(gray_level)):
+    for j in range(pixels[i]):
+        original_histogram.append(gray_level[i])
+
+new_histogram = []
+
+for i in range(len(histogram_levels)):
+    for j in range(pixels[i]):
+        new_histogram.append(histogram_levels[i])
+
+
+#original histogram
+
+plt.hist(original_histogram, bins=8, range=(0,8), edgecolor='green')
+plt.title("Original Histogram")
+plt.xlabel("Gray Level")
+plt.ylabel("Number of pixels")
+plt.show()
+
+#new histogram
+
+plt.hist(new_histogram, bins=8, range=(0,8), edgecolor='black')
+plt.title("New Histogram")
+plt.xlabel("Gray Level")
+plt.ylabel("Number of pixels")
+plt.show()
+
+
+
+
